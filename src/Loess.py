82457d78ab67d85cc54be1b2d56eb1ee65f98671
grosinov <gaston.rosinov@mercadolibre.com>
2022-07-14 12:03:23 -0300

    Muchos cambios

--- src/Loess.py
+++ src/Loess.py
@@ -22,22 +22,20 @@ def bi_square(xx, **kwargs):
     ans[idx] = (1 - xx[idx] ** 2) ** 2
     return ans
 
+def unweighted(x):
+    return np.ones_like(x)
+
 kernel_map = {
     "bisquare": bi_square,
     "tricubic": tricubic,
+    "unweighted": unweighted,
 }
 
 class Loess(object):
 
-    @staticmethod
-    def normalize_data(data):
-        mean = np.mean(data)
-        standard_deviation = np.std(data)
-        return (data - mean) / standard_deviation, mean, standard_deviation
-
     def __init__(self, xx, yy):
-        self.n_xx, self.mean_xx, self.std_xx = self.normalize_data(xx)
-        self.n_yy, self.mean_yy, self.std_yy = self.normalize_data(yy)
+        self.n_xx = xx
+        self.n_yy = yy
     
     @staticmethod
     def get_weights(distances, min_range, kernel):
@@ -48,12 +46,6 @@ class Loess(object):
     @staticmethod
     def get_min_range(distances, window):
         return np.argsort(distances, axis=0)[:window]
-
-    def normalize_x(self, x):
-        return (x - self.mean_xx) / self.std_xx
-
-    def denormalize_y(self, value):
-        return value * self.std_yy + self.mean_yy
 
     def weighted_pseudo_inverse(self, X, W):
         Xt_W = np.dot(X.T, W)
@@ -74,41 +66,19 @@ class Loess(object):
                 regression type (1 = linear, 2 = quadratic)
         """
 
-        normalized_x = self.normalize_x(x)
-
-        # distances = np.sqrt(np.sum(np.power(self.n_xx - normalized_x, 2), 1))
-        distances = np.array([np.linalg.norm(_x - normalized_x) for _x in self.n_xx])
+        distances = np.array([np.linalg.norm(_x - x) for _x in self.n_xx])
         min_range = self.get_min_range(distances, window)
         weights = self.get_weights(distances, min_range, kernel_map[kernel])
         
         # Extending matrices depending on degree
         pf = PolynomialFeatures(degree)
 
-        xp = pf.fit_transform([normalized_x])[0]
+        xp = pf.fit_transform([x])[0]
         X1 = self.n_xx[min_range]
-        X1_pre = X1.reshape(-1, 1) if len(X1.shape) == 1 else X1
         if len(X1.shape) == 1:
             X1 = pf.fit_transform(X1.reshape(-1, 1))
         else:
-            print("caca")
             X1 = pf.fit_transform(X1)
-
-        # if degree == 2:
-        #     xp = PolynomialFeatures(1).fit_transform([normalized_x])[0]
-
-        #     xpSq = np.square(normalized_x)
-
-        #     cross = PolynomialFeatures(2, interaction_only=True).fit_transform([normalized_x])[0][len(normalized_x)+1:]
-
-        #     xp = np.concatenate((np.concatenate((xp, xpSq)), cross))
-
-        #     X1 = PolynomialFeatures(1).fit_transform(X1_pre)
-
-        #     X1Sq = np.square(X1_pre)
-
-        #     X1cross = PolynomialFeatures(2, interaction_only=True).fit_transform(X1_pre)[:, len(X1_pre[0])+1:]
-
-        #     X1 = np.concatenate((np.concatenate((X1, X1Sq), axis=1), X1cross), axis=1)
 
         # Weight matrix
         W = np.diag(weights)
@@ -120,4 +90,4 @@ class Loess(object):
 
         beta = np.dot(WPinv, y)
 
-        return self.denormalize_y(np.dot(beta, xp))
+        return np.dot(beta, xp)
